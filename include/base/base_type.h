cd89125b7b7ae0c554bacae6953ec73794479a21
Kim Namil <handisu@kookmin.ac.kr>
2024-11-06 20:00:31 +0900

    ADD: Base type추가

--- include/base/base_type.h
+++ include/base/base_type.h
@@ -12,6 +12,9 @@ typedef uint8_t byte; ///< unsigned char의 별칭. BYTE와 같은 역할.
 typedef uint32_t word; ///< unsigned int의 별칭. WORD와 같은 역할. (TODO: 8, 16, 32, 64비트로 변경 가능하도록 구현)
 #elif word_size == 64
 typedef uint64_t word; ///< unsigned int의 별칭. WORD와 같은 역할. (TODO: 8, 16, 32, 64비트로 변경 가능하도록 구현)
+<<<<<<< HEAD
+#endif // __BASE_TYPE_H__
+
 typedef struct 
 {
     int8_t sign; ///< 부호
@@ -19,4 +22,3 @@ typedef struct
     word* start; ///< 정수의 절댓값을 저장하기 위한 WORD 배열의 시작 주소
 } bigint;
 
-#endif // __BASE_TYPE_H__
